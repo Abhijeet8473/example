35b1eddfd5bad789c4bd206c3713e7390a746ee7
Abhijeet8473 <abhijeetkashid84734@gmail.com>
2024-06-21 00:41:30 +0530

    added the code

--- example.rb
+++ example.rb
@@ -1,1 +1,22 @@
-puts "Hello abhijeet"
+
+def checkif(min_student, stud)
+  nega = 0
+  posti = 0
+  stud.each do |i|
+    i<=0? nega+=1:posti+=1
+  end
+
+  if (nega>min_student)
+    puts "clas is not cancelled"
+  else
+    puts "class is cancelled"
+  end
+end
+
+
+
+
+min_student = 2
+stud = [9, 3, 0, -1, 6, 2, 10, -2, -5]
+
+checkif(min_student, stud)
